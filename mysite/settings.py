c7a396c0cef38daf87e4023892123ff4546fdd3c
Alexander V. Ribchansky <avr39ripe@gmail.com>
2014-01-08 03:21:57 +0200

    admin templates customisation

--- mysite/settings.py
+++ mysite/settings.py
@@ -66,9 +66,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Uzhgorod'
 
 USE_I18N = True
 
@@ -81,3 +81,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = [os.path.join(BASE_DIR,'templates')]
