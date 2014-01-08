4f32dcfc2f025586ed0a25670c57ffe38eb02c79
Alexander V. Ribchansky <avr39ripe@gmail.com>
2014-01-08 02:35:23 +0200

    fix typo

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from polls.models import Poll, Choice
 
 # Register your models here.
+class ChoiseInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':['question']}),
+        ('Date published', {'fields':['pub_date']}),
+    ]
+    inlines = [ChoiseInline]
+
+admin.site.register(Poll,PollAdmin)
