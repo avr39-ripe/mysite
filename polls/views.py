8cd7087f60fcc91fd2ba1f6c833656a13b0bcfd6
Alexander V. Ribchansky <avr39ripe@gmail.com>
2014-01-19 16:29:16 +0200

    Class-based views added

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
+
 from polls.models import Poll, Choice
 
 # Create your views here.
+class IndexView(generic.ListView):
+        template_name = 'polls/index.html'
+        context_object_name = 'latest_poll_list'
+        def get_queryset(self):
+            return Poll.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    template_name = 'polls/detail.html'
+    model = Poll
+
+class ResultsView(generic.DetailView):
+    template_name = 'polls/results.html'
+    model = Poll
+
 def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
     output = ', '.join([p.question for p in latest_poll_list])
