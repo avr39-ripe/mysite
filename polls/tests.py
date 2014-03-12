8c6add81af738cf04a35718844c7a1683c45e025
shurik <avr39ripe@gmail.com>
2014-03-12 17:54:29 +0200

    test starting

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.utils import timezone
+
+import datetime
+from polls.models import Poll
 
 # Create your tests here.
+
+class PollMethodTest(TestCase):
+    def test_was_published_recently_with_future_poll(self):
+        """
+        was_published_recently() should return False of polls from future
+        """
+        future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
+        self.assertEqual(future_poll.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_poll(self):
+        """
+        was_published_recently() should return False for old polls
+        """
+        old_poll = Poll(pub_date=timezone.now() - datetime.timedelta(days=30))
+        self.assertEqual(old_poll.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_poll(self):
+        """
+        was_published_recently() should return True for recent polls
+        """
+        recent_poll = Poll(pub_date=timezone.now() - datetime.timedelta(hours=1))
+        self.assertEqual(recent_poll.was_published_recently(), True)
