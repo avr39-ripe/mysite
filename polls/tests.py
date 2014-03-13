2274dc566e9fa6648d27ca5b949af0557eff0a7e
shurik <avr39ripe@gmail.com>
2014-03-13 18:08:36 +0200

    testing testing testing ;)

--- polls/tests.py
+++ polls/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 from django.utils import timezone
+#import sys
 
 import datetime
 from polls.models import Poll
@@ -27,3 +29,17 @@ class PollMethodTest(TestCase):
         """
         recent_poll = Poll(pub_date=timezone.now() - datetime.timedelta(hours=1))
         self.assertEqual(recent_poll.was_published_recently(), True)
+
+def create_poll(question, days):
+    return Poll.objects.create(question=question, pub_date=timezone.now()+datetime.timedelta(deys=days))
+
+class PollViewTests(TestCase):
+    def test_index_view_with_no_polls(self):
+        """
+        If no polls exist, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('polls:index'))
+#        print >> sys.stderr, response
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls available")
+        self.assertQuerysetEqual(response.context['latest_poll_list'], [])
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.utils import timezone
 from django.views import generic
 
 from polls.models import Poll, Choice
@@ -10,7 +11,7 @@ class IndexView(generic.ListView):
         template_name = 'polls/index.html'
         context_object_name = 'latest_poll_list'
         def get_queryset(self):
-            return Poll.objects.order_by('-pub_date')[:5]
+            return Poll.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
